a41e62c870801826af1b85ab2cdfefe70d7838ee
François Michel <francois.michel@uclouvain.be>
2024-01-20 02:04:11 +0000

    add log in handler

--- handler.go
+++ handler.go
@@ -3,19 +3,24 @@ package http_signature_auth
 
 import (
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 func NewSignatureAuthHandler(keysDB *Keys, handlerFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ok, err := VerifySignature(keysDB, r)
 		if err != nil {
+			log.Debug().Msgf("error when verifying signature: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		if !ok {
+			log.Debug().Msgf("Unauthorized request from %s", r.RemoteAddr)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
+		log.Debug().Msgf("Authorized request from %s", r.RemoteAddr)
 		handlerFunc(w, r)
 	}
 }
